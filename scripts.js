2aab06385b74265b482723ddbbf688f5d379f099
wairimukuria <mimukuria@gmail.com>
2019-02-17 14:26:34 +0300

    made changes to the js file

--- scripts.js
+++ scripts.js
@@ -1,39 +1,41 @@
+// vanilla javascript
 var triangle = function(sideOne, sideTwo, sideThree){
-  if (isTriangle(sideOne, sideTwo, sideThree)) {
-    return triangleType(sideOne, sideTwo, sideThree)
-  } else {
-    return "These sides do not a triangle make.";
-  }
-};
+    if (isTriangle(sideOne, sideTwo, sideThree)) {
+      return triangleType(sideOne, sideTwo, sideThree)
+    } else {
+      return "These sides do not make a triangle .";
+    }
+  };
 
-var triangleType = function(sideOne, sideTwo, sideThree){
-  if (sideOne===sideTwo && sideTwo===sideThree) {
-    return "equilateral";
-  } else if (sideOne===sideTwo || sideOne===sideThree || sideThree===sideTwo) {
-    return "isosceles";
-  } else {
-    return "scalene";
-  }
-};
+  var triangleType = function(sideOne, sideTwo, sideThree){
+    if (sideOne===sideTwo && sideTwo===sideThree) {
+      return "equilateral";
+    } else if (sideOne===sideTwo || sideOne===sideThree || sideThree===sideTwo) {
+      return "isosceles";
+    } else {
+      return "scalene";
+    }
+  };
 
-var isTriangle = function(sideOne, sideTwo, sideThree) {
-  return sideOne <= (sideTwo + sideThree) && sideTwo <= (sideThree + sideOne) && sideThree <= (sideOne + sideTwo)
-};
-
-$(document).ready(function() {
-  $("form#triangle").submit(function(event) {
-
-    var sideOne = parseInt($("input#sideOne").val());
-    var sideTwo = parseInt($("input#sideTwo").val());
-    var sideThree = parseInt($("input#sideThree").val());
-    var result = triangle(sideOne, sideTwo, sideThree);
-
-    sideOne = parseInt($("input#sideOne").val(""));
-    sideTwo = parseInt($("input#sideTwo").val(""));
-    sideThree = parseInt($("input#sideThree").val(""));
-
-    $("#result").append(result);
-    event.preventDefault();
-    alert("result");
-  });
-});
+  var isTriangle = function(sideOne, sideTwo, sideThree) {
+    return sideOne <= (sideTwo + sideThree) && sideTwo <= (sideThree + sideOne) && sideThree <= (sideOne + sideTwo)
+  };
+  window.onload = function() {
+    //get the form from the html doc
+    var form = document.querySelector("form");
+    //add listener for the submit action
+    form.addEventListener("submit", function(event) {
+        //get the triangle side values
+      var sOne = form.elements[0].value;
+      var sTwo = form.elements[1].value;
+      var sThree = form.elements[2].value;
+      //compute the value to get triangle type
+      var result = triangleType(sOne, sTwo, sThree);
+      //display the result on the screen h3#result
+      document.getElementById('result').innerHTML=result;
+      //reset the form to empty fields
+      form.reset();
+      // prevent page refresh for the submit event
+      event.preventDefault();
+    });
+  };
