438a4b6ac7472933b149343c15fa47b140e1b3f5
wairimukuria <mimukuria@gmail.com>
2019-02-19 14:45:53 +0300

    made changes to the javascript files

--- scripts.js
+++ scripts.js
@@ -1,25 +1,25 @@
-var triangle = function(){
+var triangle = function() {
   var sideOne = parseInt(document.getElementById("sideOne").value);
   var sideTwo = parseInt(document.getElementById("sideTwo").value);
   var sideThree = parseInt(document.getElementById("sideThree").value);
   if (isTriangle(sideOne, sideTwo, sideThree)) {
-    return triangeType(sideOne,sideTwo,sideThree)
-  }else {
+    return triangeType(sideOne, sideTwo, sideThree)
+  } else {
     return alert("these sides do not make a triangle.");
   }
 };
 
-var triangleType = function(sideOne,sideTwo,sideThree){
-  if ((sideOne === sideTwo) &&(sideTwo === sideThree)){
-    return alert("Equilateral:All sides of the triangle are equal.");
-  }else if ((sideOne === sideTwo) || (sideOne === sideThree) ||(sideThree === sideTwo)){
-    return alert("Isosceles: All sides of the triangle are equal.");
-  }else{
+var triangleType = function(sideOne, sideTwo, sideThree) {
+  if ((sideOne === sideTwo) && (sideTwo === sideThree)) {
+    return alert("Equilateral : All sides of the triangle are equal.");
+  } else if ((sideOne === sideTwo) || (sideOne === sideThree) || (sideThree === sideTwo)) {
+    return alert("Isosceles: two sides of the triangle are equal.");
+  } else {
     return alert("Scalene: None of the sides of the triange are equal."):
   }
 };
 
 
-var isTriangle = function(sideOne,sideTwo,sideThree){
+var isTriangle = function(sideOne, sideTwo, sideThree) {
   return (sideOne <= (sideTwo + sideThree)) && (sideTwo <= (sideThree + sideOne)) && (sideThree <= (sideOne + sideTwo));
 }
